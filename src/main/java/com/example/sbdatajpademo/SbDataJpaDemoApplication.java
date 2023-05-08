340e578ed4b77296d1093a311091408027671f48
nazarchykur <nazarchykur@gmail.com>
2023-05-08 15:54:02 +0300

    init

---
+++ src/main/java/com/example/sbdatajpademo/SbDataJpaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbdatajpademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbDataJpaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbDataJpaDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sbdatajpademo/SbDataJpaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbdatajpademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbDataJpaDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
